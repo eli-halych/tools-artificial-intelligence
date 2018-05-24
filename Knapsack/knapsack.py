5bc6dce4d421bcb3d410fe2eabbdad95c237a7b2
Illia Halych <middlezwei@gmail.com>
2018-05-24 16:28:59 +0200

    The solution for a knapsack problem using binary characteristic(indicator) vector. The only thing the left here is to input items from the file, but generally speaking the algorithm is done. Also, I had to reverse the indexes, because many zeros at the beginning contract to just one zero

--- Knapsack/knapsack.py
+++ Knapsack/knapsack.py
@@ -1,7 +1,7 @@
-import matplotlib
-import matplotlib.pyplot as plt
+import math
 
 
+# here i run out of memory if #of items is 30
 def powerset(items):
     res = [[]]
     for item in items:
@@ -9,36 +9,61 @@ def powerset(items):
         res.extend(newset)
     return res
 
-def indicator_vector(items):
-    pass
+
+def indicator_vector(num):
+    return bin(num)[2:]
+
 
 def characteristic_vector(items):
     res = []
 
+
 def knapsack_brute_force(items, max_weight):
     knapsack = []
+    n = len(items)
     best_weight = 0
     best_value = 0
-    for item in items:
-        for j in range(len(item)):
-            
+    bin_range = range(0, int(math.pow(2, n)))
+    bag = []
 
+    for i in bin_range:
+        b = indicator_vector(i)
+        w_sum = 0
+        v_sum = 0
 
-        # set_weight = sum(map(weight, item_set))
-        # set_value = sum(map(value, item_set))
-        # if set_value > best_value and set_weight <= max_weight:
-        #     best_weight = set_weight
-        #     best_value = set_value
-        #     knapsack = item_set
+        # ------------REVERSING INDICES FOR A BINARY NUMBER, testing
+        # x = n - len(b)
+        # # print("x", x)
+        # for k in range(len(b)):
+        #     current_index = ((len(b)+x)-k)-1
+        #     if current_index > 0 and b != '0':
+        #         print("index = ", current_index, "for binary = ", b)
+        # print("------------------------")
+        # ------------
+
+        num_zeros_before = n - len(b)
+        for j in range(len(b)):
+            current_index = ((len(b) + num_zeros_before) - j) - 1
+            # 0 is a useless case
+            if b != '0':
+
+                if b[j] == '1':
+                    w_sum += items[current_index][1]
+                    v_sum += items[current_index][2]
+
+        if w_sum <= max_weight and v_sum > best_value and b != '0':
+            best_weight = w_sum
+            best_value = v_sum
+            bag = []
+            for e in range(len(b)):
+                curr_ind = ((len(b) + num_zeros_before) - j) - 1
+                if b[e] == '1':
+                    bag.append(items[curr_ind])
+            print("Probable knapsack:", bag)
+
+        knapsack = bag
+
     return knapsack, best_weight, best_value
-
-
-def weight(item):
-    return item[1]
-
-
-def value(item):
-    return item[2]
 
 
 def build_items(n):
@@ -49,9 +74,7 @@ def build_items(n):
     return res
 
 
-# items = [(0,2,4), (1,5,3), (2,7,4), (3,3,5)]
-n = 10
-print("Computations =", 2**n)
+n = 10 # number of items
 items = build_items(n)
 max_weight = 20
 knapsack, opt_wt, opt_val = knapsack_brute_force(items, max_weight)
